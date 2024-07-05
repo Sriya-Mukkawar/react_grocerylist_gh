9f3f5e56a92e3e6a658afa3da67df3b2e4bf7514
Sriya Mukkawar <mukkawarsriya@gmail.com>
2024-07-05 23:44:27 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import { useState, useEffect } from 'react';
 import apiRequest from './apiRequest';
 
 function App() {
-  const API_URL = 'http://localhost:3500/items';
+  const API_URL = 'https://my-json-server.typicode.com/Sriya-Mukkawar/react_grocerylist_gh/items';
 
   const [items, setItems] = useState([]);
   const [newItem, setNewItem] = useState('');
